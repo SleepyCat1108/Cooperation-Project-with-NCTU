1584ee76868b488ccd85eab8c484b390ae98b08f
Felix Tien <76773035+SleepyCat1108@users.noreply.github.com>
2021-08-28 23:47:25 +0800

    Add files via upload

---
+++ Part1/pin_config.h
@@ -0,0 +1,19 @@
+#ifndef PIN_CONFIG_H
+#define PIN_CONFIG_H
+
+#define PIN_ENABLE  NRF_GPIO_PIN_MAP(1,1)
+#define PIN_B0      NRF_GPIO_PIN_MAP(1,2)
+#define PIN_B1      NRF_GPIO_PIN_MAP(1,3)
+#define PIN_B2      NRF_GPIO_PIN_MAP(1,4)
+#define PIN_B3      NRF_GPIO_PIN_MAP(1,5)
+#define PIN_B4      NRF_GPIO_PIN_MAP(1,6)
+#define PIN_B5      NRF_GPIO_PIN_MAP(1,7)
+#define PIN_B6      NRF_GPIO_PIN_MAP(1,8)
+#define PIN_B7      NRF_GPIO_PIN_MAP(1,9)
+#define PIN_B8      NRF_GPIO_PIN_MAP(1,10)
+#define PIN_B9      NRF_GPIO_PIN_MAP(1,11)
+#define PIN_B10     NRF_GPIO_PIN_MAP(1,12)
+#define PIN_B11     NRF_GPIO_PIN_MAP(1,13)
+
+
+#endif 
